328547e7b320bf51694ddd9a74495cabf3896cee
Renan Lopes <renanlopescoder@gmail.com>
2017-03-24 20:57:24 -0300

    feat: Wikipedia API
    Added API Wikipedia to search articles by location searched.

--- js/script.js
+++ js/script.js
@@ -17,10 +17,14 @@ function loadData() {
 
     $greeting.text('you want to live at ' + streetViewAddress + ' ?');
 
+    // Google Maps API
+
     var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
      + streetViewAddress;
     
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
+
+    // New York Times API
 
     var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
      + cityString + "&sort=newest&api-key=5093abc88ed5407caa1c1a2bad0a816d";
@@ -35,6 +39,23 @@ function loadData() {
         };
     }).error(function(error){
         $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+    });
+
+    // Wikipedia API
+
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+ cityString + "&format=json&callback=wikiCallback";
+
+    $.ajax({
+        url : wikiUrl,
+        dataType : "jsonp",
+        success: function ( response ){
+            var articleList = response[1];
+            for(var i = 0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                var url = "http://en.wikipedia.org/wiki/"+articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr +'</a></li>');
+            }
+        }
     });
 
     return false;
