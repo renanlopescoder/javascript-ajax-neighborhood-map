965ba8b4cf7137ac097ab1f71b35e1724d8f0c07
Renan Lopes <renanlopescoder@gmail.com>
2017-03-24 17:59:42 -0300

    feat: New York Times API
    Added API New York Times to search articles by location searched.

--- js/script.js
+++ js/script.js
@@ -22,6 +22,21 @@ function loadData() {
     
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
 
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
+     + cityString + "&sort=newest&api-key=5093abc88ed5407caa1c1a2bad0a816d";
+     
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text("New York Times Articles About " + cityString);
+        articles = data.response.docs;
+        for(var i = 0; i < articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+            '<a href="'+ article.web_url+'">'+article.headline.main+'</a><p>'+article.snippet+'</p>'+'</li>');
+        };
+    }).error(function(error){
+        $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
+    });
+
     return false;
 };
 
