3eaa677bceca8f241ecb931eba25bfd9020fa328
Renan Lopes <renanlopescoder@gmail.com>
2017-03-27 11:45:03 -0300

    feat: Material Design applied
    Added responsive design and material design with "materializecss"

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function loadData() {
 
-    var $body = $('body');
+    var $body = $('#maps');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
@@ -13,53 +13,53 @@ function loadData() {
 
     var streetString = $('#street').val();
     var cityString = $('#city').val();
-    var streetViewAddress = streetString + ', '+cityString;
+    var streetViewAddress = streetString + ', ' + cityString;
 
     $greeting.text('you want to live at ' + streetViewAddress + ' ?');
 
     // Google Maps API
 
     var streetViewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
-     + streetViewAddress;
-    
-    $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
+        + streetViewAddress;
+
+    $body.append('<img class="col s12 m12 l12" src="' + streetViewUrl + '">');
 
     // New York Times API
 
     var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="
-     + cityString + "&sort=newest&api-key=5093abc88ed5407caa1c1a2bad0a816d";
-     
-    $.getJSON(nytimesUrl, function(data){
+        + cityString + "&sort=newest&api-key=5093abc88ed5407caa1c1a2bad0a816d";
+
+    $.getJSON(nytimesUrl, function (data) {
         $nytHeaderElem.text("New York Times Articles About " + cityString);
         articles = data.response.docs;
-        for(var i = 0; i < articles.length; i++){
+        for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+
-            '<a href="'+ article.web_url+'">'+article.headline.main+'</a><p>'+article.snippet+'</p>'+'</li>');
+            $nytElem.append('<li class="article">' +
+                '<a href="' + article.web_url + '">' + article.headline.main + '</a><p>' + article.snippet + '</p>' + '</li>');
         };
-    }).error(function(error){
+    }).error(function (error) {
         $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
     });
 
     // Wikipedia API
 
-    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+ cityString + "&format=json&callback=wikiCallback";
+    var wikiUrl = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityString + "&format=json&callback=wikiCallback";
 
-    var timeoutWiki = setTimeout(function(){
+    var timeoutWiki = setTimeout(function () {
         $wikiElem.text("Sorry, we failed to get wikipedia articles (Timeout Error) please check your internet connection and try again.");
     }, 8000);
 
     $.ajax({
-        url : wikiUrl,
-        dataType : "jsonp",
-        success: function ( response ){
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function (response) {
             var articleList = response[1];
-            for(var i = 0; i < articleList.length; i++){
+            for (var i = 0; i < articleList.length; i++) {
                 articleStr = articleList[i];
-                var url = "http://en.wikipedia.org/wiki/"+articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr +'</a></li>');
+                var url = "http://en.wikipedia.org/wiki/" + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
             };
-            
+
             clearTimeout(timeoutWiki);
         }
     });
